db1f677685b04dd7fc13c7ebc722b260fc29ed9f
Peter <xutongle@gmail.com>
2017-11-24 11:23:56 +0800

    utf8mb4

--- migrations/M171114032653Create_authentication_table.php
+++ migrations/M171114032653Create_authentication_table.php
@@ -12,7 +12,7 @@ class M171114032653Create_authentication_table extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
         }
         $this->createTable('{{%authentications}}', [
             'user_id' => $this->integer()->unsigned()->notNull()->comment('User ID'),
